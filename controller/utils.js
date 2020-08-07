4827c173109932b68c0c816015599e5c7bd65f48
xubing <xubing@yiche.com>
2020-08-07 09:10:30 +0800

    nothing

--- controller/utils.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: Bing Xu
- * @Date: 2020-06-07 15:27:05
- * @LastEditTime: 2020-06-07 15:27:05
- * @LastEditors: Bing Xu
- * @Description: 
- * @FilePath: /node-practice/controller/utils.js
- */ 
