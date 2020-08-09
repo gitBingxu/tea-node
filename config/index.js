d8134ee38cef917867be0d7c62dc04704d33e756
xubing <xubing@yiche.com>
2020-08-09 15:25:23 +0800

    update

--- config/index.js
+++ config/index.js
@@ -1,13 +1,13 @@
 /*
  * @Author: Bing Xu
  * @Date: 2020-06-07 15:45:37
- * @LastEditTime: 2020-06-07 20:08:42
- * @LastEditors: Bing Xu
+ * @LastEditTime: 2020-08-09 11:53:29
+ * @LastEditors: Please set LastEditors
  * @Description: app的配置
  * @FilePath: /node-practice/config/index.js
  */ 
 
 module.exports = {
   port: 8080,
-  db: "mysql://root:root@localhost:3306/node_practice?timezone=+8000"
+  db: "mysql://root:123456@localhost:3306/node_practice?timezone=+8000"
 }
