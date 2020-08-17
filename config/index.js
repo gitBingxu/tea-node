e998dd76bb7c3a25207d12efb15f57fd8a4744a8
xubing <xubing@yiche.com>
2020-08-17 18:14:07 +0800

    [add] pm2 deploy.yaml

--- config/index.js
+++ config/index.js
@@ -1,13 +1,13 @@
 /*
  * @Author: Bing Xu
  * @Date: 2020-06-07 15:45:37
- * @LastEditTime: 2020-08-09 11:53:29
+ * @LastEditTime: 2020-08-17 18:06:54
  * @LastEditors: Please set LastEditors
  * @Description: app的配置
  * @FilePath: /node-practice/config/index.js
  */ 
 
 module.exports = {
-  port: 8080,
+  port: 3000,
   db: "mysql://root:123456@localhost:3306/node_practice?timezone=+8000"
 }
